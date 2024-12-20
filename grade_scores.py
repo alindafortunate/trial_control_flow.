67de46f8158a0071c1e6618bb4c2d4764fcebc7b
alindafortunate <alindafortunate5@gmail.com>
2024-12-20 11:16:40 +0300

    Initial commit

---
+++ grade_scores.py
@@ -0,0 +1,3 @@
+age = int(input("Enter your age: "))
+message='Eligible' if age>=18 else 'Not eligible'
+print(message)
---
+++ vote.py
@@ -0,0 +1,7 @@
+# A program to check vote eligibility.
+age = int(input("Enter our age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
